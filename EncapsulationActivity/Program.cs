ba1c2532a62d006f9794029c6740643467983986
namita1409 <81958036+namita1409@users.noreply.github.com>
2022-05-20 19:33:16 +0100

    second commit

--- EncapsulationActivity/Program.cs
+++ EncapsulationActivity/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using EncapsulationActivity.Models;
+
+Console.WriteLine("Welcome to the school!");
+
+Student student = new Student("Namita", GRADE.B, 3);
+Console.WriteLine(student.Grade);
+if (student.Grade == GRADE.B)
+    student.Grade = GRADE.A;
+if (student.Grade == GRADE.C)
+    student.Grade = GRADE.A;
+
+Console.WriteLine(student.Group);
+Console.WriteLine(student.Grade);
